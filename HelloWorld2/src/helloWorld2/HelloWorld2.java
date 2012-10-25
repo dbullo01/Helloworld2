67e8f5de28af8be5d73a652838f437a18e3cd2ab
Davison Bullock <dbullo01@mail.bbk.ac.uk>
2012-10-25 12:42:46 +0100

    Changed message in HelloWorld2 class to say "Hello my Name is Davison!" message

--- HelloWorld2/src/helloWorld2/HelloWorld2.java
+++ HelloWorld2/src/helloWorld2/HelloWorld2.java
@@ -13,7 +13,7 @@ public class HelloWorld2 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello my Name is Davison!");
 
 	}
 
